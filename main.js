dbaeeb4367ed2a148853a831a98e21cf0221becd
tusharkumar2302 <tusharkumar27864@gmail.com>
2025-01-20 14:10:45 +0530

    Initial commit for Trip Report project

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    },
+});
+    mainWindow.loadFile('index.html'); // Load your app's UI
+});
